e98c270a2232fe5e0974134838b7f9c6fc052e07
Fredrik Lund <fredrik.lund89@gmail.com>
2022-02-15 15:22:10 +0100

    Changes to email configuration

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+require('dotenv').config({})
+
 const fastify = require('fastify')({
   logger: true
 })
 
+//
+//  Setup Nodemailer
+//
+
 const Mailer = require('nodemailer')
+
+let transporter = Mailer.createTransport({
+  sendmail: true,
+  newline: 'unix',
+  path: '/usr/bin/sendmail',
+  secure: true,
+  dkim: {
+    domainName: 'fredriklund.se',
+    keySelector: 'default',
+    privateKey: process.env.DKIM_PRIVATE_KEY
+  }
+})
 
 //
 //  Functions
@@ -34,10 +52,19 @@ fastify.get('/', async (request, reply) => {
 
 fastify.post('/email/send', async (request, reply) => {
   try {
-    const body = request.body
-
-    if (!body) throw await onError(400, 'Bad request', 'Missing request body')
-
+    console.log(request.body)
+    if (!request.body) throw await onError(400, 'Bad request', 'Missing request body')
+    console.log(request)
+    //transporter.sendMail({
+    //  to: 'fredrik.lund89@gmail.com',
+    //  from: 'noreply@fredriklund.se',
+    //  subject: 'Testing mail function with Nodemailer',
+    //  text: 'This is my message in text',
+    //  html: '<h1>Hi</h1><br /><p>This is my message in HTML</p>'
+    //})
+    reply.send({
+      foo: 'bar'
+    })
   }
   catch (error) {
     await reply.code(error.code || 500).send(error)
