6640ce43b5041cdbbf45e96a3c0d796c725dbccb
Fredrik Lund <fredrik.lund89@gmail.com>
2022-02-15 18:03:57 +0100

    Adjusted transporter settings in attempt to fix DKIM

--- server.js
+++ server.js
@@ -53,9 +53,7 @@ fastify.get('/api/email', async (request, reply) => {
 // Won't work in local dev
 fastify.post('/api/email/send', async (request, reply) => {
   try {
-    console.log(request.body)
     if (!request.body) throw await onError(400, 'Bad request', 'Missing request body')
-    console.log(request)
     let info = await transporter.sendMail({
       from: request.body.from, // Can also be used with alias, such as '"Fredrik Lund" <email@fredriklund.se>'
       to: request.body.to, // Recipient, can be sent to multiple addresses using comma as separator
@@ -65,6 +63,7 @@ fastify.post('/api/email/send', async (request, reply) => {
     })
     reply.send({
       message_id: info.messageId,
+      envelope: info.envelope,
       from: request.body.from,
       to: request.body.to,
       subject: request.body.subject
